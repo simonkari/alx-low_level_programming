897cb3986cd8af983198248c3456fa0eb6f4bc2b
simonkari <kariithisimon1715@gmail.com>
2023-02-16 10:44:38 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,11 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
+ * The main function returns an integer value, which represents the exit status of the program. In this case, we simply return 0 to indicate that the program executed successfully
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
 
