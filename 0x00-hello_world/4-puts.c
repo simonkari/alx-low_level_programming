7e6d993f73c3709efe555e9f45651a60dccd59bc
simonkari <kariithisimon1715@gmail.com>
2023-02-16 10:26:25 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,9 @@
 /**
  * main - A program that prints a line with put fnction
  * return: 0 (Successs)
+ * In this program, we use the puts function to print the required message to the console. The puts function automatically adds a new line at the end of the message, so we don't need to add it ourselves.
+
+The main function returns an integer value, which represents the exit status of the program. In this case, we simply return 0 to indicate that the program executed successfully.
  */
 int main(void)
 {
