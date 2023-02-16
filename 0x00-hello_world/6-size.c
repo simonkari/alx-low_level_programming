945bbba00b90aeacd1268be5abaef23ab6094c1d
simonkari <kariithisimon1715@gmail.com>
2023-02-16 12:58:34 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,5 +19,5 @@ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0); // Return value of 0 indicates success
+return (0); /* Return value of 0 indicates success*/
 }
