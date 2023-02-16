cfad0ba4249150464c150657b5e13966365d9001
simonkari <kariithisimon1715@gmail.com>
2023-02-16 11:56:31 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 /* Use printf to printi message with proper grammar */
-printf("%s, %s\n", "with proper grammar", "but the outcome is a piece of art");
+printf("%s, %s\n", "with proper grammar, but the outcome is a piece of art,");
 /* Return 0 to indicate program success */
 return (0);
 }
