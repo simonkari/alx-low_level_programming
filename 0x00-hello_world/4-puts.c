fcdc315c4060ab97de718c4247a9ce873d7c2816
simonkari <kariithisimon1715@gmail.com>
2023-02-16 10:38:09 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints a line with put fnction
- * return: 0 (Successs)
- * The main function returns an integer value, which represents the exit status of the program. In this case, we simply return 0 to indicate that the program executed successfully.
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
 }
+
