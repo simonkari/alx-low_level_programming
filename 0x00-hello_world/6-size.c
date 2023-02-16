28a3d970ef1f4bc46561c657bf2c640ff8932987
simonkari <kariithisimon1715@gmail.com>
2023-02-16 12:43:45 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,6 +3,7 @@
  * main - A program that prints the size of various computer types
  * Return 0 (Success)
  * The main function returns an integer, return 0 to indicate success.
+ * The main function returns an integer value, which represents the exit status of the program
  */
 int main(void)
 {
