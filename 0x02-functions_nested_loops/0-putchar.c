39229004109cfbb633ac0e550b0a0d7482259974
simonkari <kariithisimon1715@gmail.com>
2023-02-21 17:04:41 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,9 +6,7 @@
 */
 
 int main(void)
-
 {
-
     _putchar('_');
     _putchar('p');
     _putchar('u');
@@ -18,6 +16,5 @@ int main(void)
     _putchar('a');
     _putchar('r');
     _putchar('\n');
-    return (0);
-    
+    return (0);  
 }
