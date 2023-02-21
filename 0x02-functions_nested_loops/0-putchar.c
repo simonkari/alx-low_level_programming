6ea3a085201b7863669d1879feaf7bbf7a9a0572
simonkari <kariithisimon1715@gmail.com>
2023-02-21 17:34:36 +0300

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - Entry point
  * Return: Always 0
@@ -16,5 +15,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return ('0\n');
+return (0);
 }
