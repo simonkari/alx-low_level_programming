2cb5a21b252ff601222d8447a1e5ce18ce571169
simonkari <kariithisimon1715@gmail.com>
2023-02-23 20:58:40 +0300

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - print the largest prime factor of 6127852475143
+ * Return: Always (0)
+*/
+
+int main(void)
+{
+long x, maxf;
+long number = 612852475243;
+double square = sqrt(number);
+
+for (x = 1; x <= square; x++)
+{
+if (number % x == 0)
+{
+maxf = number / x;
+}
+}
+printf("%ld\n", maxf);
+return (0);
+}
