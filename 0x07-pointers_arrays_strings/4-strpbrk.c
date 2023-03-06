7d231e9064975b36c826701d7090d12150d6bfb9
simonkari <kariithisimon1715@gmail.com>
2023-03-06 17:47:57 +0300

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,27 +11,27 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i;
-	int j = 0;
-	int check = 0;
-	char *p = NULL;
+int i;
+int j = 0;
+int check = 0;
+char *p = NULL;
 
-	for (i = 0; s[i]; i++)
-	{
-		for (j = 0; accept[j]; j++)
-		{
-			check = 0;
-			if (s[i] == accept[j])
-			{
-				p = (s + i);
-				check = 1;
-				break;
-			}
-		}
-		if (check == 1)
-		{
-			break;
-		}
-	}
-	return (p);
+for (i = 0; s[i]; i++)
+{
+for (j = 0; accept[j]; j++)
+{
+check = 0;
+if (s[i] == accept[j])
+{
+p = (s + i);
+check = 1;
+break;
+}
+}
+if (check == 1)
+{
+break;
+}
+}
+return (p);
 }
