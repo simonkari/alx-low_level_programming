faf281bfce3bdbf8d45945cf8723d37149b4657c
simonkari <kariithisimon1715@gmail.com>
2023-03-08 19:44:42 +0300

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,6 +6,10 @@
  * description: _strlen_recursion - Returns Length of String
  * return: integer value
  * On error, -1 is returned, and errno is set appropriately.
+ * ecursively computes the length of a string.
+ *
+ * @param str a pointer to a string
+ * @return the length of the string, or 0 if the string is empty
 */
 
 int _strlen_recursion(char *s)
