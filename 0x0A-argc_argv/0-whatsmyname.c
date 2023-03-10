18eb2401d57c3a247048ac7b0663a3a2d0015ed4
simonkari <kariithisimon1715@gmail.com>
2023-03-10 22:26:01 +0300

    Write a program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,18 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - print its name
- * @argc: argument count
- * @argv: argument vector
- * return:always zero
-*/
+ * main - program that prints its name, followed by a new line
+ *
+ * @argc: argument count for main
+ * @argv: vector to the arguments
+ *
+ * Return: void
+ */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-(void) argc;
-printf("%s\n", argv[0]);
-
-return (0);
+	if (argc >= 1)
+		printf("%s\n", argv[0]);
+	return (0);
 }
