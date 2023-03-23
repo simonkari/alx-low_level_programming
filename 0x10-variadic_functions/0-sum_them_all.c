6610c5a2450feddf1b7303d4bb391e9786f677af
simonkari <kariithisimon1715@gmail.com>
2023-03-23 21:45:51 +0300

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,13 +1,11 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
-
 /**
  * sum_them_all - Entry Point
  * @n: variadic inputs
  * Return: sum
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list valist;
