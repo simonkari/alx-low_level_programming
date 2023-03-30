108c796fdb9bd767a735bd41a84d27bfde413451
simonkari <kariithisimon1715@gmail.com>
2023-03-30 21:30:45 +0300

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,33 @@
+/*
+ * File: 0-print_list.c
+ * Auth: Brennan D Baraban
+ */
+
 #include "lists.h"
+#include <stdio.h>
 
 /**
-  * print_list - fills memory with a constant byte
-  * @h: is the owner of the dog
-  * Return: a number
-  */
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
+ *
+ * Return: The number of nodes in h.
+ */
 
 size_t print_list(const list_t *h)
 {
-size_t i;
+	size_t nodes = 0;
 
-for (i = 0; h != NULL; i++)
-{
-if (h->str != NULL)
-{
-printf("[%d] %s\n", h->len, h->str);
-h = h->next;
-}
-else
-{
-printf("[%d] %s\n", 0, "(nil)");
-h = h->next;
-}
-}
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 
-return (i);
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
 }
