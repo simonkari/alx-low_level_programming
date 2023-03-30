3211168d7f3d249bc594e644f5b52272725217fb
simonkari <kariithisimon1715@gmail.com>
2023-03-30 20:11:35 +0300

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,11 +5,12 @@
   * @h: is the owner of the dog
   * Return: a number
   */
+
 size_t list_len(const list_t *h)
 {
-	size_t i;
+size_t i;
 
-	for (i = 0; h != NULL; i++)
-		h = h->next;
-	return (i);
+for (i = 0; h != NULL; i++)
+h = h->next;
+return (i);
 }
