36058836b67bf19ce14fb5ca34ead69d961af887
simonkari <kariithisimon1715@gmail.com>
2023-05-18 23:52:16 +0300

    6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -4,7 +4,6 @@
  * get_dnodeint_at_index - Locates a node
  * @head: Head of the dlistint_t list.
  * @index: node to locate.
- *
  * Return: If the node does not exist - NULL.
  *         Otherwise - the address of the located node.
  */
