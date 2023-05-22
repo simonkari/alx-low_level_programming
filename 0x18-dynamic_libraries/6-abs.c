75eb12c3414753a2a5fd74e0f8c3da292f9b21f5
simonkari <kariithisimon1715@gmail.com>
2023-05-22 20:29:43 +0300

    6-abs.c

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _abs - returns the absolute value of an integer
+* @n: the string you want to convert
+* Return: Returns the integer value of n
+*/
+int _abs(int n)
+{
+if (n < 0)
+return (n * -1);
+return (n);
+}
