6dab254a0c528dced881b7f7a194509030e3f70a
simonkari <kariithisimon1715@gmail.com>
2023-05-22 23:07:50 +0300

    task 1

--- 0x18-dynamic_libraries/0-memset.c
+++ 0x18-dynamic_libraries/0-memset.c
@@ -12,12 +12,15 @@
  * Return: A pointer to the filled memory area @s.
  */
 
-void *_memset(void *s, int c, size_t n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
+    unsigned char *p = s;
+    unsigned char uc = b;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
-	return (memory);
+    while (n--)
+    {
+        *p++ = uc;
+    }
+    
+    return s;
 }
