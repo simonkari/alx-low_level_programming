b36fa846f085833ab71d272f534a1ad992a2540e
simonkari <kariithisimon1715@gmail.com>
2023-05-22 20:39:02 +0300

    math.c

---
+++ 0x18-dynamic_libraries/math.c
@@ -0,0 +1,22 @@
+int add(int s, int k)
+{
+	return (s + k);
+}
+int sub(int s, int k)
+{
+	return (s - k);
+}
+
+int mod(int s, int k)
+{
+	return (s % k);
+}
+
+int mul(int s, int k)
+{
+	return (s * k);
+}
+int div(int s, int k)
+{
+	return (s / k);
+}
