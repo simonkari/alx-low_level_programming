18790bf72b3175390b8004cca9e81747bc1023ee
simonkari <kariithisimon1715@gmail.com>
2023-05-22 19:05:23 +0300

    0-isupper.c

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,10 @@
+/**
+ * _isupper - prints if the characters are lowercase or not
+ * 0 lowercase 1 uppercase
+ * @c: the character tested
+ * Return: 0 lowercase, 1 uppercase
+ */
+int _isupper(int c)
+{
+    return (c >= 'A' && c <= 'Z');
+}
