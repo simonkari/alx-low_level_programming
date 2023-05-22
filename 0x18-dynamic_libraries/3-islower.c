bdaaf00204964efd7d325f672c03146e49b7ad12
simonkari <kariithisimon1715@gmail.com>
2023-05-22 20:03:33 +0300

    3-islower.c

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* _islower  - prints if characters are lowercase or not
+* 0 for not lowercase, 1 for lowercase
+* @c: the letter being tested for case
+* Return: 0 if not lowercase, 1 if lowercase
+*/
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+return (0);
+}
