bf0a53fc51f95d32839a1e3ac609dd57d12a4a1d
simonkari <kariithisimon1715@gmail.com>
2023-05-22 20:16:52 +0300

    4-isalpha.c

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _isalpha - returns 1 or 0
+* depending on if the character is capitalized or not.
+* @c: the character that is looked at.
+* Return: Returns 1 if capital, 0 if not.
+*/
+int _isalpha(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+return (0);
+}
