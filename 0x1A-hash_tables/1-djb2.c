2ac6dda2c9877c2f15e9d05ac66b494b3879db25
simonkari <kariithisimon1715@gmail.com>
2023-06-08 22:14:36 +0300

    1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -5,13 +5,14 @@
  * @str: The string used to generate the hash value
  * Return: The resulting hash value
  */
-unsigned long int hash_djb2(const unsigned char *str) {
-    unsigned long int hash = 5381;
-    int c;
-
-    while ((c = *str++)) {
-        hash = ((hash << 5) + hash) + c; // hash * 33 + c
-    }
-
-    return hash;
+unsigned long int hash_djb2(const unsigned char *str)
+{
+unsigned long int hash;
+int c;
+hash = 5381;
+while ((c = *str++))
+{
+	hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+}
+return (hash);
 }
