d5b55b2ccc4b851a6fe95e5d2c4ac4b1cb46ee6a
simonkari <kariithisimon1715@gmail.com>
2023-07-26 20:27:41 +0300

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""The function typically works by iterating through
+each cell in the grid and checking its neighboring
+cells to determine whether it is part of the island's
+boundary or not."""
+
+
+def island_perimeter(grid):
+    """Calculater the Island's perimeter.
+
+    The grid represents water by 0 and land by 1.
+
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
